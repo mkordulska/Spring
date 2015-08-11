c7d9add4d9214cc1e95ce8a7587726b1ec4d2647
Marta Kordulska <marta.kordulska@capgemini.com>
2015-08-11 15:20:44 +0200

    LibraryDaoImplTest added

---
+++ spring-data-jpa/src/test/java/pl/spring/demo/dao/LibraryDaoImplTest.java
@@ -0,0 +1,33 @@
+package pl.spring.demo.dao;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import pl.spring.demo.entity.LibraryEntity;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "CommonDaoTest-context.xml")
+public class LibraryDaoImplTest {
+
+	@Autowired
+	LibraryDao libraryDao;
+
+    @Test
+    public void testShouldFindLibraryByName() {
+        // given
+        final String libraryName = "bibl";
+        // when
+        List<LibraryEntity> libraryEntity = libraryDao.findLibraryByName(libraryName);
+        // then
+        assertNotNull(libraryEntity);
+        assertFalse(libraryEntity.isEmpty());
+    }
+
+}
